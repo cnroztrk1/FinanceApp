e4485daba1c9156344c6c6218721259181746c21
Caner Öztürk <cnroztrk1@gmail.com>
2025-02-16 13:22:02 +0300

    Değerledirme ekranı eklendi.

---
+++ FinanceApp/Controllers/FinanceReportController.cs
@@ -0,0 +1,56 @@
+﻿using FinanceApp.Business.Services;
+using FinanceApp.Common;
+using FinanceApp.Data.Entities;
+using FinanceApp.Presentation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceApp.Presentation.Controllers
+{
+    public class FinanceReportController : Controller
+    {
+        private readonly IJobsService _jobService;
+        private readonly IAgreementService _agreementService;
+        private readonly IRiskAnalysisService _riskAnalysisService;
+        private readonly ITenantProvider _tenantProvider;
+
+        public FinanceReportController(
+            IJobsService jobService,
+            IAgreementService agreementService,
+            IRiskAnalysisService riskAnalysisService,
+            ITenantProvider tenantProvider)
+        {
+            _jobService = jobService;
+            _agreementService = agreementService;
+            _riskAnalysisService = riskAnalysisService;
+            _tenantProvider = tenantProvider;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var tenantId = _tenantProvider.TenantId;
+
+            // Mali Verileri Al
+            var jobs = await _jobService.GetAllJobsAsync();
+            var agreements = await _agreementService.GetAllAgreementsAsync();
+            var risks = await _riskAnalysisService.GetAllRiskAnalysesAsync();
+
+            var viewModel = new FinanceReportViewModel
+            {
+                TotalJobs = jobs.Count(j => j.TenantId == tenantId),
+                TotalAgreements = agreements.Count(a => a.TenantId == tenantId),
+                TotalRiskAmount = risks.Where(r => r.TenantId == tenantId).Sum(r => r.RiskAmount),
+                RiskAnalysisData = risks.Where(r => r.TenantId == tenantId)
+                                        .GroupBy(r => r.AnalysisDate.Year)
+                                        .Select(g => new RiskAnalysisYearlyData
+                                        {
+                                            Year = g.Key,
+                                            TotalRisk = g.Sum(r => r.RiskAmount)
+                                        }).ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ FinanceApp/Models/FinanceReportViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace FinanceApp.Presentation.Models
+{
+    public class FinanceReportViewModel
+    {
+        public int TotalJobs { get; set; }
+        public int TotalAgreements { get; set; }
+        public decimal TotalRiskAmount { get; set; }
+        public List<RiskAnalysisYearlyData> RiskAnalysisData { get; set; }
+    }
+
+    public class RiskAnalysisYearlyData
+    {
+        public int Year { get; set; }
+        public decimal TotalRisk { get; set; }
+    }
+}
